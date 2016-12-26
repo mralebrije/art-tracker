e6196eb037e726c823e63f1f6ebed65e92c9512f
Sergio Duran <serch_sddm@outlook.com>
2016-12-26 15:23:37 -0600

    Customize ui-grid

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,12 +1,16 @@
 (function() {
-	var artTrackerApp = angular.module('artTrackerApp', ['ngTouch', 'ui.grid', 'ui.grid.selection']);
+	var artTrackerApp = angular.module('artTrackerApp', ['ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.pagination']);
 
 	artTrackerApp.factory('ApiFactory', function($http) {
 		return {
 			getMuseums : function() {
 				return $http.get('/art-tracker/museum', {
 				});
-			}
+			},
+			postMuseum : function() {
+            				return $http.post('/art-tracker/museum', {
+            				});
+            }
 		}
 	});
 
@@ -16,31 +20,41 @@
 	function MuseumController($scope, $interval, ApiFactory) {
 		var reloadInterval = 1200000;
 
-		// variables
+		// VARIABLES
 		angular.extend($scope, {
-			museums : [],
 			gridOptions : {
+			    columnDefs: [
+                    { name:'Name', field: 'name', headerCellClass: 'purple'},
+                    { name:'Postal Code', field: 'zip' },
+                    { name:'Neighborhood', field: 'neighborhood' },
+                    { name:'Council District', field: 'council_district'},
+                    { name:'Police District', field: 'police_district'},
+                    { name:'Address', field: 'address'},
+                    { name: 'Actions', enableFiltering: false, enableSorting: false,
+                      cellTemplate:'<button   type="button" class="btn btn-warning btn-customized" ng-click="grid.appScope.editMuseum()">Edit</button> <button   type="button" class="btn btn-danger btn-customized" ng-click="grid.appScope.deleteMuseum()">Delete</button>'  }
+			    ],
+			    paginationPageSize: 10,
                 enableSorting: true,
                 enableRowSelection: true,
                 enableFullRowSelection: true,
                 multiSelect: true,
-                enableRowHeaderSelection: false,
-                enableColumnMenus: false,
+                enableRowHeaderSelection: true,
+                enableColumnMenus: true,
                 enableFiltering: true,
                 showGridFooter: true,
                 showColumnFooter: true
             },
+            hideGrid:false,
 			error : false
 		});
 
-		// methods
+		// METHODS
 		angular.extend($scope, {
-			retrieveMuseums : retrieveMuseums
+			retrieveMuseums : retrieveMuseums,
+			addMuseum : addMuseum,
+			editMuseum : editMuseum,
+			deleteMuseum : deleteMuseum
 		});
-
-
-
-
 
 		function retrieveMuseums() {
 			ApiFactory.getMuseums().then(
@@ -52,11 +66,39 @@
 					});
 		}
 
+		function addMuseum() {
+        			ApiFactory.postMuseum().then(
+        					function(response) {
+        						$scope.gridOptions.data =  response.data.museums;;
+
+        						$scope.gridOpts.data.push({
+                                                "Name": "New ",
+                                                "Postal Code": "Person ",
+                                                "Neighborhood": "abc",
+                                                "Council District": true,
+                                                "Police District": "male",
+                                                "Address": "male"
+                                });
+
+        					}, function(error) {
+        						handleApiError(error);
+        					});
+        		}
+
+        function editMuseum() {
+            alert("editMuseum");
+        }
+
+        function deleteMuseum() {
+            alert("deleteMuseum");
+		}
+
 		function handleApiError(error) {
+		            alert("Error in REST service")
         			if (error.config.errorCause === 1) {
         				//do something
         			}
-        		}
+        }
 
 		function initialize() {
 				$scope.error = false;
